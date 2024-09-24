0595c0751d6317ad7f6016a191933677d5935a93
Karmalized <Matthew.H.Tran-1@ou.edu>
2024-09-24 00:40:08 +0000

    project items

---
+++ memory.c
@@ -0,0 +1,122 @@
+/* Name: Matthew Tran, Username: cs141 (cs141@cs141.cs.ourcloud.ou.edu)*/
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+/* Note for Reference: Key - used for identifying shared memory segments */
+/* It allows you access to pieces of shared memory found in the system */
+
+/* key number stored here */
+key_t generated_key;
+
+/* this is where the values for shared memory is stored */
+typedef struct
+{
+int value;
+} shared_mem;
+
+shared_mem *total;
+
+/*all process are called in their own functions and they increment to by a certain amount as listed below*/
+/*once they do that they exit their process and after exiting their certain process each process terminates*/
+int process1(){
+for(int i = 0; i < 100000; i++){
+	total->value++;
+}
+printf("From Child Process 1: counter: %d.\n", total->value);
+return 0; 
+}
+
+int process2(){
+for(int i = 0; i < 100000; i++){
+	total->value++;
+}
+printf("From Child Process 2: counter: %d.\n", total->value);
+return 0;
+}
+
+int process3(){
+for(int i = 0; i < 100000; i++){
+	total->value++;
+}
+printf("From Child Process 3: counter: %d.\n", total->value);
+return 0;
+}
+
+int process4(){
+for(int i = 0; i < 200000; i++){
+	total->value++;
+}
+printf("From Child Process 4: counter: %d.\n", total->value);
+return 0;
+}
+
+int main(){
+	generated_key = ftok("./",'a');
+	int shmid, pid1, pid2, pid3, pid4, ID, status;
+	char * shmadd;
+	shmadd = (char*) 0;
+	
+	/* Create and connect to a shared memory segment*/
+	/*If there is an issue with obtaining or attaching the key then throw error*/
+	if((shmid = shmget(generated_key, sizeof(int), IPC_CREAT | 0666)) < 0){
+		perror("shmget");
+		exit(1);}
+
+	if((total = (shared_mem *) shmat (shmid,shmadd,0)) == (shared_mem *)-1){
+		perror("shmat");
+		exit(0);}
+
+	/* shared memory is initialized to zero */
+	total->value = 0;
+
+	/* Now that the key, shared memory, and variables are setup lets create */
+	/* some processes! */
+	if((pid1 = fork()) == 0){
+		process1();
+		exit(0);
+	} else {
+		waitpid(pid1, NULL, 0);
+		printf("Child with pid %d has just exited.\n", pid1);
+	}
+	if ((pid2 = fork()) == 0){
+		process2();
+		exit(0);
+	} else {
+		wait(NULL);
+		printf("child with PID: %d has just exited.\n", pid2);
+	}
+
+	if((pid3 = fork()) == 0){
+		process3();
+		exit(0);
+	} else {
+		wait(NULL);
+		printf("child with PID %d has just exited.\n", pid3);
+	}
+	if((pid4 = fork()) == 0){
+		process4();
+		exit(0);
+	} else {
+		wait(NULL);
+		printf("child with PID %d has just exited.\n", pid4);
+	}
+
+	/*after each process increments and terminates, we detach and remove shared memory here*/
+
+	if(shmdt(total) == 1){
+		perror("shmdt");
+		exit(-1);
+	}
+
+	shmctl(shmid, IPC_RMID, NULL);
+
+	printf("\n End of simulation :)");
+
+
+}	
